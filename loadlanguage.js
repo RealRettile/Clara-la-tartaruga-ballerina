b1b2c2f3621eeae3e9b5f8e9ddac2ba179218b39
Rettile <reptilian.discord@gmail.com>
2024-08-06 18:14:16 +0200

    Update loadlanguage.js

--- loadlanguage.js
+++ loadlanguage.js
@@ -1,4 +1,18 @@
-const client = require('./bot')
+const path = require('path');
+const config = require('./config.js'); 
+
+const langPath = path.join(__dirname, 'languages', `${config.language}.js`);
+let lang;
+
+try {
+  lang = require(langPath);
+} catch (err) {
+  console.error(`Language file for ${config.language} not found.`, err);
+  process.exit(1);
+}
+
+module.exports = lang;
+
 
 /*
 
