69c99b151b423e0d7e497a6b0c5853340903b5be
Rettile <reptilian.discord@gmail.com>
2024-08-07 16:20:11 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,137 +1,75 @@
-/*
-
-  ________.__                        _____.___.___________
- /  _____/|  | _____    ____  ____   \__  |   |\__    ___/
-/   \  ___|  | \__  \ _/ ___\/ __ \   /   |   |  |    |   
-\    \_\  \  |__/ __ \\  \__\  ___/   \____   |  |    |   
- \______  /____(____  /\___  >___  >  / ______|  |____|   
-        \/          \/     \/    \/   \/                  
-
-╔════════════════════════════════════════════════════════════════════════╗
-║                                                                        ║
-║  ## Created by GlaceYT!                                                ║
-║  ## Feel free to utilize any portion of the code                       ║
-║  ## DISCORD :  https://discord.com/invite/xQF9f9yUEM                   ║
-║  ## YouTube : https://www.youtube.com/@GlaceYt                         ║
-║                                                                        ║
-╚════════════════════════════════════════════════════════════════════════╝
-
-
-*/
-
-
-
-const { Client, GatewayIntentBits, Partials } = require("discord.js");
-const { DisTube } = require("distube");
-const { SpotifyPlugin } = require("@distube/spotify");
-const { SoundCloudPlugin } = require("@distube/soundcloud");
-const { DeezerPlugin } = require("@distube/deezer");
-const { YtDlpPlugin } = require("@distube/yt-dlp");
-const { printWatermark } = require('./util/pw');
-const config = require("./config.js");
-const fs = require("fs");
+require('dotenv').config();
+const { Client, GatewayIntentBits, Collection } = require('discord.js');
+const fs = require('fs');
+const config = require('./config');
+const { DisTube } = require('distube');
+const { SpotifyPlugin } = require('@distube/spotify');
+const { YtDlpPlugin } = require('@distube/yt-dlp');
+const { SoundCloudPlugin } = require('@distube/soundcloud');
+const { DeezerPlugin } = require('@distube/deezer');
+const { YouTubePlugin } = require('@distube/youtube');
+const { DirectLinkPlugin } = require('@distube/direct-link');
 const path = require('path');
 
 const client = new Client({
-  intents: Object.keys(GatewayIntentBits).map((a) => {
-    return GatewayIntentBits[a];
-  }),
-});
-
+    intents: [
+      GatewayIntentBits.Guilds,
+      GatewayIntentBits.GuildVoiceStates,
+      GatewayIntentBits.GuildMessages,
+      GatewayIntentBits.MessageContent,
+    ],
+  });
+  
+client.commands = new Collection();
 client.config = config;
-client.player = new DisTube(client, {
-  leaveOnStop: config.opt.voiceConfig.leaveOnStop,
-  leaveOnFinish: config.opt.voiceConfig.leaveOnFinish,
-  leaveOnEmpty: config.opt.voiceConfig.leaveOnEmpty.status,
-  emitNewSongOnly: true,
-  emitAddSongWhenCreatingQueue: false,
-  emitAddListWhenCreatingQueue: false,
-  plugins: [
-    new SpotifyPlugin(),
-    new SoundCloudPlugin(),
-    new YtDlpPlugin(),
-    new DeezerPlugin(),
-  ],
-});
-process.env.YTDL_NO_UPDATE = true;
-const player = client.player;
 
-fs.readdir("./events", (_err, files) => {
-  files.forEach((file) => {
-    if (!file.endsWith(".js")) return;
-    const event = require(`./events/${file}`);
-    let eventName = file.split(".")[0]; 
+fs.readdirSync('./commands').forEach(file => {
+    const command = require(`./commands/${file}`);
+    client.commands.set(command.data.name, command);
+});
+
+const eventFiles = fs.readdirSync(path.join(__dirname, 'events')).filter(file => file.endsWith('.js'));
+
+eventFiles.forEach(file => {
+  const event = require(`./events/${file}`);
+  const eventName = file.split('.')[0];
+  
+  if (typeof event === 'function') {
     client.on(eventName, event.bind(null, client));
-    delete require.cache[require.resolve(`./events/${file}`)];
-  });
-});
-fs.readdir("./events/player", (_err, files) => {
-  files.forEach((file) => {
-    if (!file.endsWith(".js")) return;
-    const player_events = require(`./events/player/${file}`);
-    let playerName = file.split(".")[0];
-    player.on(playerName, player_events.bind(null, client));
-    delete require.cache[require.resolve(`./events/player/${file}`)];
-  });
-});
-
-client.commands = [];
-fs.readdir(config.commandsDir, (err, files) => {
-  if (err) throw err;
-  files.forEach(async (f) => {
-    try {
-      if (f.endsWith(".js")) {
-        let props = require(`${config.commandsDir}/${f}`);
-        client.commands.push({
-          name: props.name,
-          description: props.description,
-          options: props.options,
-        });
-      }
-    } catch (err) {
-      console.log(err);
-    }
-  });
-});
-
-
-
-if (config.TOKEN || process.env.TOKEN) {
-  client.login(config.TOKEN || process.env.TOKEN).catch((e) => {
-    console.log('TOKEN ERROR❌❌');
-  });
-} else {
-  setTimeout(() => {
-    console.log('TOKEN ERROR❌❌');
-  }, 2000);
-}
-
-
-if(config.mongodbURL || process.env.MONGO){
-  const mongoose = require("mongoose")
-  mongoose.connect(config.mongodbURL || process.env.MONGO, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  }).then(async () => {
-    console.log('\x1b[32m%s\x1b[0m', `|    🍔 Connected MongoDB!`)
-  }).catch((err) => {
-    console.log('\x1b[32m%s\x1b[0m', `|    🍔 Failed to connect MongoDB!`)})
   } else {
-  console.log('\x1b[32m%s\x1b[0m', `|    🍔 Error MongoDB!`)
+    console.error(`Event handler in ${file} is not a function.`, event);
   }
+});
 
+client.distube = new DisTube(client, {
+    plugins: [
+        new YouTubePlugin(),     // Supports YouTube
+        new SpotifyPlugin(),     // Supports Spotify
+        new SoundCloudPlugin(),  // Supports SoundCloud
+        new DeezerPlugin(),      // Supports Deezer
+        new DirectLinkPlugin(),  // Supports direct audio links
+        new YtDlpPlugin(),       // Supports 700+ sites including YouTube
+    ],
+});
+
+require('./events/distubeEvents')(client);
 
 const express = require("express");
 const app = express();
 const port = 3000;
 app.get('/', (req, res) => {
-  const imagePath = path.join(__dirname, 'index.html');
-  res.sendFile(imagePath);
+    const imagePath = path.join(__dirname, 'index.html');
+    res.sendFile(imagePath);
 });
 app.listen(port, () => {
-  console.log(`🔗 Listening to GlaceYT: http://localhost:${port}`);
+    console.log(`🔗 Listening to GlaceYT : http://localhost:${port}`);
 });
-printWatermark();
+
+
+client.login(process.env.TOKEN);
+
+module.exports = client;
+
 
 /*
 
